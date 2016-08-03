b008d3174c6a02fedbc80ca094798485dedf89c4
Simon <s.komlos@gmail.com>
2016-08-03 19:16:11 -0400

    Skeleton of the components

---
+++ components/index/main-controller.js
@@ -0,0 +1,10 @@
+angular.module('MainController', [
+        'MainService',
+        'MainDirective',
+    ])
+    .controller('MainCtrl', function($scope, $http, Main) {
+
+        var self = this;
+        const NASA_USER_ID = "24662369@N07";
+
+    });
